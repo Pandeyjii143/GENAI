c39284cc2f11ccdc3d82351f1d5f01172d046e20
Ravikishan Pandey <pandeyravi2143@gmail.com>
2026-07-22 10:46:44 +0530

    dynamic Prompt added

--- 4.Prompt/4.Prompt.py
+++ 4.Prompt/4.Prompt.py
@@ -1,15 +1,50 @@
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
 import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
 
 load_dotenv()
 model = ChatOpenAI()
 
 st.header("Research Tool")
-user_input = st.text_input("Enter your Prompt")
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
 
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = PromptTemplate(
+    template="""
+Please summarize the research paper titled "{paper_input}" with the following specifications.
+
+Explanation style:
+{style_input}
+
+Explanation length:
+{length_input}
+
+1. Mathematical Details:
+- Include relevant mathematical equations if present.
+- Explain them in simple language.
+- Include code snippets where applicable.
+
+2. Analogies:
+- Use relatable analogies to explain complex ideas.
+
+If certain information is unavailable, respond with
+"Insufficient information available" instead of guessing.
+""",
+    input_variables=["paper_input","style_input","length_input"]
+)
+
+#fill the placeholder
+prompt = template.invoke({
+    "paper_input": paper_input,
+    "style_input": style_input,
+    "length_input": length_input
+})
 if st.button('Summarize'):
-    result = model.invoke(user_input)
+    result = model.invoke(prompt)
     st.write(result.content)
 
 
